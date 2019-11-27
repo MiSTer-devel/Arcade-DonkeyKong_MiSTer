d30698ae73c6a3994efa647dda9cdd40390903dc
Alan Steremberg <alan.steremberg@gmail.com>
2019-11-26 19:23:41 -0800

    added gamma

--- Arcade-DonkeyKong.sv
+++ Arcade-DonkeyKong.sv
@@ -142,6 +142,9 @@ wire [10:0] ps2_key;
 wire [15:0] joystick_0, joystick_1;
 wire [15:0] joy = joystick_0 | joystick_1;
 
+wire [21:0] gamma_bus;
+
+
 hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 (
 	.clk_sys(clk_sys),
@@ -152,6 +155,7 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.buttons(buttons),
 	.status(status),
 	.forced_scandoubler(forced_scandoubler),
+	.gamma_bus(gamma_bus),
 
 	.ioctl_download(ioctl_download),
 	.ioctl_wr(ioctl_wr),
